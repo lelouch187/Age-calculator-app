ce99c8daa1daa0e8ca0cde7f036ab0b26321617f
lelouch187 <vanushs187@gmail.com>
2023-05-20 15:20:41 +0600

    add fonts and icon

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
+import './fonts.scss';
 import App from './App';
-
+import './assets/fonts/Poppins/Poppins-Bold.ttf';
+import './assets/fonts/Poppins/Poppins-BoldItalic.ttf';
+import './assets/fonts/Poppins/Poppins-ExtraBold.ttf';
+import './assets/fonts/Poppins/Poppins-ExtraBoldItalic.ttf';
+import './assets/fonts/Poppins/Poppins-Italic.ttf';
+import './assets/fonts/Poppins/Poppins-Regular.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
