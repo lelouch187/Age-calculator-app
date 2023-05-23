2f69f612df5be772cdd48a0b3bfc37110ed8117d
lelouch187 <vanushs187@gmail.com>
2023-05-23 20:59:57 +0600

    bag fix

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ export const isError = (inputValue) => {
   for (let key in inputValue) {
     if (inputValue[key] === '') err = { ...err, [key]: 'This field if required' };
   }
-  if (new Date()<new Date(inputValue.years, inputValue.month, inputValue.day))
+  if (new Date()<new Date(inputValue.years, inputValue.month-1, inputValue.day))
     err = { ...err, years: 'Must be in the past' };
   if (inputValue.month > 12) err = { ...err, month: 'Must be a valid month' };
   if (inputValue.day > new Date(inputValue.years, inputValue.month, 0).getDate())
